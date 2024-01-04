2bf59605fabdec2f09a9a87bdab64a6c3fbf67a8
RanitBehera <ranitbehera1998@gmail.com>
2024-01-05 00:55:13 +0530

    Milestone Mid

--- script.js
+++ script.js
@@ -13,69 +13,86 @@ function TabClick(sender){
 }
 
 
-// --- CARD SPECIFIC
-function ProcessPlayerCards(){
-    cards = document.getElementsByClassName("card players");
-    for (c=0; c<cards.length;c++){
-        card = cards[c];
 
-        html = ""
-        players = card.children
 
-        for (p=0;p<players.length;p++){
-            player = players[p]
-            html += player.outerHTML
+shelf_content = document.getElementsByClassName("shelf")[0].getElementsByClassName("content")[0]
 
-            // Add Icon
-            html += "<div class=\"icon material-symbols-outlined\">";
-                // player_class=card.className.split(' ');
-                player_class=card.children[p].className.split(' ');
-                // player_class=card.getElementsByClassName("name")[0].className.split(' );
 
-                if (player_class.includes("male")){html += "face";}
-                else if(player_class.includes("female")){html += "face_3";}
-                else{html += "no_accounts";}
-            html += "</div>"
-        }
-        
-        card.innerHTML = html;
+// By Anirban
+function Gender_Icon(gender){
+    if (gender.toLowerCase() == 'male') {
+        icon = "face";
+    } else if (gender.toLowerCase() == 'female') {
+        icon = "face_3";
+    } else {
+        icon = "transgender";
+    }
+    return icon;
+}
+
+function AddSection(section_name){
+    html = "<div class=\"card section span2\">"+section_name+"</div>";
+    shelf_content.innerHTML += html;
+}
+
+function AddText(text){
+    html = "<div class=\"card text span2 \">" + text+"</div>";
+    shelf_content.innerHTML += html;
+}
+
+
+function AddSinglePlayer(name, gender){
+    icon = Gender_Icon(gender);
+    html  = "<div class=\"card players single\">";
+    html += "<div class=\"name\">"+name+"</div>";
+    html += "<div class=\"icon material-symbols-outlined\">"+icon+"</div>";
+    html += "</div>";
+    shelf_content.innerHTML += html;
+}
+
+function AddDoublePlayer(name1, gender1, name2, gender2) {
+    icon1 = Gender_Icon(gender1);
+    icon2 = Gender_Icon(gender2);
+    html  = "<div class=\"card players double\">";
+    html += "<div class=\"name\">"+name1+"</div>";
+    html += "<div class=\"icon material-symbols-outlined\">"+icon1+"</div>";
+    html += "<div class=\"name\">"+name2+"</div>";
+    html += "<div class=\"icon material-symbols-outlined\">"+icon2+"</div>";
+    html += "</div>";
+    shelf_content.innerHTML += html;
+}
+
+function AddCategory(single_or_double, gender, category_name, live) {
+    html = "<div class=\"card game-category "+single_or_double+" "+gender+"\">";
+    html += "<div class=\"text\">";
+    html += "<div class=\"name\">"+ category_name +"</div>";
+    if (live == true){
+        html += "<div ><span class=\"live\">LIVE</span></div>";
+    } else {
+        html += "<div ><span class=\"live hide\">LIVE</span></div>";
+    }
+    html += "</div>";
+    html += "<div class=\"icon material-symbols-outlined\">";
+    // <!-- Implement control by here -->
+    // <!-- Right now by CSS -->
+    // <!-- Alignment Issue via HTML -->
+    html += " </div>"; /* Add at least one character (even whitespace) otherwise css letter-spacing will break*/
+    html += "</div>";
+    shelf_content.innerHTML += html;
+}
+
+// 
+// 
+function ChangeTheme(){
+    console.log("Test");
+    document.body.classList.toggle("theme-light");
+    document.body.classList.toggle("theme-dark");
+
+    if(document.body.classList.split(' ').includes("theme-dark")){
+        document.body.style.backgroundColor="black";
+    }else{
+        document.body.style.backgroundColor="white";
     }
 }
-ProcessPlayerCards()
-
-
-
-function ProcessCategoryCards(){
-    cards = document.getElementsByClassName("card category");
-    for (c=0; c<cards.length;c++){
-        card = cards[c];
-        html = card.innerHTML
-            
-        card_class=card.className.split(' ');
-        // Add Icon
-        /*
-        if (card_class.includes("single")){
-            html += "<span class=\"icon material-symbols-outlined\">";
-            if (card_class.includes("men")){html += "man";}
-            else if(card_class.includes("women")){html += "woman";}
-            else{html += "man";}
-            html += "</span>"
-        }else if (card_class.includes("double")){
-            for (i=0;i<2;i++){
-                html += "<span class=\"icon material-symbols-outlined\">";
-                if (card_class.includes("men")){html += "man";}
-                else if(card_class.includes("women")){html += "woman";}
-                else{html += "man";}
-                html += "</span>"
-            }
-        }else{
-
-        }
-        */
-        html += "<span class=\"icon material-symbols-outlined\"></span>";
-
-        card.innerHTML = html;
-    }
-        
-}
-ProcessCategoryCards()
+// 
+// 
